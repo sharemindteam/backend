a4cbccdfe0244082d36b20f988c7a851609b0fa7
aeyongdodam <kikititi0510@gmail.com>
2023-10-22 01:35:25 +0900

    feat: Message 엔티티 생성

---
+++ src/main/java/com/example/sharemind/message/domain/Message.java
@@ -0,0 +1,35 @@
+package com.example.sharemind.message.domain;
+
+import com.example.sharemind.consult.domain.Consult;
+import com.example.sharemind.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Message extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private Long messageId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "consult_id")
+    private Consult consult;
+
+    private Boolean isSender;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder
+    public Message(Consult consult, Boolean isSender, String content) {
+        this.consult = consult;
+        this.isSender = isSender;
+        this.content = content;
+    }
+}

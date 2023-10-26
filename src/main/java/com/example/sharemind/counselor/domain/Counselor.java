05ab186cba76640b547f38326721ef08e87d404b
letskuku <yujung9288@naver.com>
2023-10-27 01:11:20 +0900

    feat: 상담사, 사용자 정보에 저장할 필드 추가

--- src/main/java/com/example/sharemind/counselor/domain/Counselor.java
+++ src/main/java/com/example/sharemind/counselor/domain/Counselor.java
@@ -1,10 +1,7 @@
 package com.example.sharemind.counselor.domain;
 
 import com.example.sharemind.global.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,15 +14,25 @@ public class Counselor extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "counselor_id")
     private Long counselorId;
-
-    private String email;
 
     private String nickname;
 
+    private String email;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String accountNumber;
+
     @Builder
-    public Counselor(String email, String nickname) {
-        this.email = email;
+    public Counselor(String nickname, String email, String name, String phoneNumber, String accountNumber) {
         this.nickname = nickname;
+        this.email = email;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.accountNumber = accountNumber;
     }
 }

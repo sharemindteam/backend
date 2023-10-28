d148eba80e1ed9bb0d9d6c7f3b8841d2fab04446
aeyongdodam <kikititi0510@gmail.com>
2023-10-29 03:01:16 +0900

    #2 fix: 메세지 저장 함수 status 리턴으로 수정

--- src/main/java/com/example/sharemind/message/presentation/MessageController.java
+++ src/main/java/com/example/sharemind/message/presentation/MessageController.java
@@ -4,6 +4,8 @@ import com.example.sharemind.consult.application.ConsultService;
 import com.example.sharemind.message.application.MessageService;
 import com.example.sharemind.message.dto.request.PostNewReq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     private final MessageService messageService;
     private final ConsultService consultService;
-    @PostMapping("/new")
-    public void saveMessage(@RequestBody PostNewReq postNewReq){
+    @PostMapping
+    public ResponseEntity<Void> createMessage(@RequestBody PostNewReq postNewReq){
         messageService.saveMessage(postNewReq);
         Boolean isSender = postNewReq.getIsSender();
         if (Boolean.TRUE.equals(isSender)) {
@@ -24,5 +26,6 @@ public class MessageController {
         } else if (Boolean.FALSE.equals(isSender)) {
             consultService.notifyConsultationReply(postNewReq.getConsultUuid());
         }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }

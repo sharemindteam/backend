b8f7be7de27b2ec9f7ae7e3c0e922c48fd9a2bec
aeyongdodam <kikititi0510@gmail.com>
2023-10-31 01:54:45 +0900

    #2 chore: consult exception 형식 수정

--- src/main/java/com/example/sharemind/consult/application/ConsultServiceImpl.java
+++ src/main/java/com/example/sharemind/consult/application/ConsultServiceImpl.java
@@ -21,7 +21,6 @@ public class ConsultServiceImpl implements ConsultService {
     private final CustomerRepository customerRepository;
     private final CounselorRepository counselorRepository;
     private final ConsultRepository consultRepository;
-    private final EmailService emailService;
 
     @Override
     public UUID createConsult(CreateConsultRequest createConsultRequest) {
@@ -34,34 +33,5 @@ public class ConsultServiceImpl implements ConsultService {
         Consult consult = consultRepository.save(createConsultRequest.toConsult(customer, counselor));
 
         return consult.getConsultUuid();
-    }
-
-    public void sendConsultationLink(UUID consult_uuid) {
-        Consult consult = consultRepository.findByConsultUuid(consult_uuid)
-                .orElseThrow(ConsultNotFoundException::new);
-        sendEmailToCustomer(consult, "상담 작성용 링크입니다.", "[sharemind]상담 링크입니다.");
-    }
-
-    public void notifyConsultationApply(UUID consult_uuid) {
-        Consult consult = consultRepository.findByConsultUuid(consult_uuid)
-                .orElseThrow(ConsultNotFoundException::new);
-        sendEmailToCustomer(consult, "상담 신청이 완료되었습니다.", "[sharemind]상담 신청이 완료되었습니다.");
-        sendEmailToCounselor(consult, "새로운 상담 신청이 있습니다.", "[sharemind]새로운 상담 신청이 있습니다.");
-    }
-
-    public void notifyConsultationReply(UUID consult_uuid) {
-        Consult consult = consultRepository.findByConsultUuid(consult_uuid)
-                .orElseThrow(ConsultNotFoundException::new);
-        sendEmailToCustomer(consult, "상담이 응답되었습니다.", "[sharemind]상담 응답 링크입니다.");
-    }
-
-    private void sendEmailToCustomer(Consult consult, String text, String subject) {
-        String emailContent = emailService.setText(text, consult.getPassword(), consult.getConsultUuid());
-        emailService.sendEmail(consult.getCustomer().getEmail(), subject, emailContent);
-    }
-
-    private void sendEmailToCounselor(Consult consult, String text, String subject) {
-        String emailContent = emailService.setText(text, consult.getPassword(), consult.getConsultUuid());
-        emailService.sendEmail(consult.getCounselor().getEmail(), subject, emailContent);
     }
 }

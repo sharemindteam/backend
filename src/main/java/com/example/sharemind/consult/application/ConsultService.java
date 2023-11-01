857b922814df0eb06af37ae5dc2f5f6ac72ac6c2
letskuku <yujung9288@naver.com>
2023-11-02 02:28:09 +0900

    #1 feat: getConsult(상담 정보 조회) 메서드 ConsultService 인터페이스에 추가 및 구현체 구현

--- src/main/java/com/example/sharemind/consult/application/ConsultService.java
+++ src/main/java/com/example/sharemind/consult/application/ConsultService.java
@@ -3,7 +3,11 @@ package com.example.sharemind.consult.application;
 import java.util.UUID;
 
 import com.example.sharemind.consult.dto.request.CreateConsultRequest;
+import com.example.sharemind.consult.dto.request.GetConsultRequest;
+import com.example.sharemind.consult.dto.response.ConsultResponse;
 
 public interface ConsultService {
     UUID createConsult(CreateConsultRequest createConsultRequest);
+
+    ConsultResponse getConsult(UUID consultUuid, GetConsultRequest getConsultRequest);
 }
--- src/main/java/com/example/sharemind/consult/application/ConsultServiceImpl.java
+++ src/main/java/com/example/sharemind/consult/application/ConsultServiceImpl.java
@@ -2,15 +2,22 @@ package com.example.sharemind.consult.application;
 
 import com.example.sharemind.consult.domain.Consult;
 import com.example.sharemind.consult.dto.request.CreateConsultRequest;
+import com.example.sharemind.consult.dto.request.GetConsultRequest;
+import com.example.sharemind.consult.dto.response.ConsultResponse;
+import com.example.sharemind.consult.exception.ConsultNotFoundException;
+import com.example.sharemind.consult.exception.IncorrectPasswordException;
 import com.example.sharemind.consult.repository.ConsultRepository;
 import com.example.sharemind.counselor.domain.Counselor;
 import com.example.sharemind.counselor.exception.CounselorNotFoundException;
 import com.example.sharemind.counselor.repository.CounselorRepository;
 import com.example.sharemind.customer.domain.Customer;
 import com.example.sharemind.customer.repository.CustomerRepository;
+import com.example.sharemind.message.dto.response.MessageResponse;
+import com.example.sharemind.message.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,6 +27,7 @@ public class ConsultServiceImpl implements ConsultService {
     private final CustomerRepository customerRepository;
     private final CounselorRepository counselorRepository;
     private final ConsultRepository consultRepository;
+    private final MessageRepository messageRepository;
 
     @Override
     public UUID createConsult(CreateConsultRequest createConsultRequest) {
@@ -32,5 +40,23 @@ public class ConsultServiceImpl implements ConsultService {
         Consult consult = consultRepository.save(createConsultRequest.toConsult(customer, counselor));
 
         return consult.getConsultUuid();
+    }
+
+    @Override
+    public ConsultResponse getConsult(UUID consultUuid, GetConsultRequest getConsultRequest) {
+
+        Consult consult = consultRepository.findByConsultUuid(consultUuid)
+                .orElseThrow(() -> new ConsultNotFoundException(consultUuid));
+
+        if (!consult.getPassword().equals(getConsultRequest.getPassword())) {
+            throw new IncorrectPasswordException();
+        }
+
+        List<MessageResponse> messageResponses = messageRepository.findAllByConsult(consult)
+                .stream()
+                .map(MessageResponse::from)
+                .toList();
+
+        return ConsultResponse.from(consult, messageResponses);
     }
 }

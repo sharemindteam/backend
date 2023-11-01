e19ea63dee358b628f329a5dc67012f2c90e0025
letskuku <yujung9288@naver.com>
2023-11-02 02:27:27 +0900

    #1 feat: MessageRepository에서 상담에 대한 대화 목록 조회 기능 추가

--- src/main/java/com/example/sharemind/message/repository/MessageRepository.java
+++ src/main/java/com/example/sharemind/message/repository/MessageRepository.java
@@ -1,10 +1,14 @@
 package com.example.sharemind.message.repository;
 
+import com.example.sharemind.consult.domain.Consult;
 import com.example.sharemind.message.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    List<Message> findAllByConsult(Consult consult);
 }

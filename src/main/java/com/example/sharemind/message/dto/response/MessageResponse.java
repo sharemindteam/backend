ca1b6f4963f0110bb5794f0073b3e20ef6dec490
letskuku <yujung9288@naver.com>
2023-11-03 01:17:21 +0900

    #1 feat: MessageResponse dto에 생성시간 필드 추가

--- src/main/java/com/example/sharemind/message/dto/response/MessageResponse.java
+++ src/main/java/com/example/sharemind/message/dto/response/MessageResponse.java
@@ -4,22 +4,27 @@ import com.example.sharemind.message.domain.Message;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MessageResponse {
 
     private final Boolean isCustomer;
     private final String content;
+    private final LocalDateTime createdAt;
 
     @Builder
     public MessageResponse(Boolean isCustomer, String content, LocalDateTime createdAt) {
         this.isCustomer = isCustomer;
         this.content = content;
+        this.createdAt = createdAt;
     }
 
     public static MessageResponse from(Message message) {
         return MessageResponse.builder()
                 .isCustomer(message.getIsCustomer())
                 .content(message.getContent())
+                .createdAt(message.getCreatedAt())
                 .build();
     }
 }

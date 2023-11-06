d03a71aff9b9233e3cfe753f1a06d1c2f3c5bc91
letskuku <yujung9288@naver.com>
2023-11-07 00:27:39 +0900

    #1 feat: 상담 신청 시 사용자 이메일만 입력받으므로 Customer 생성자, CreateConsultRequest 필드 수정

--- src/main/java/com/example/sharemind/consult/dto/request/CreateConsultRequest.java
+++ src/main/java/com/example/sharemind/consult/dto/request/CreateConsultRequest.java
@@ -1,42 +1,10 @@
 package com.example.sharemind.consult.dto.request;
 
-import com.example.sharemind.consult.domain.Consult;
-import com.example.sharemind.counselor.domain.Counselor;
-import com.example.sharemind.customer.domain.Customer;
 import lombok.Getter;
-
-import java.util.Random;
-import java.util.UUID;
 
 @Getter
 public class CreateConsultRequest {
 
     private Long counselorId;
     private String email;
-    private String name;
-    private String phoneNumber;
-    private String accountNumber;
-    private String password;
-
-    public Customer toCustomer() {
-        return Customer.builder()
-                .nickname("사용자" + new Random().nextInt(9999))
-                .email(email)
-                .name(name)
-                .phoneNumber(phoneNumber)
-                .accountNumber(accountNumber)
-                .build();
-    }
-
-    public Consult toConsult(Customer customer, Counselor counselor) {
-        return Consult.builder()
-                .customer(customer)
-                .counselor(counselor)
-                .consultUuid(UUID.randomUUID())
-                .password(password)
-                .isPay(false)
-                .isRefund(false)
-                .isReply(false)
-                .build();
-    }
 }

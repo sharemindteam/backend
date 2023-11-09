e42fcfcb1c556b56188b0a8be0773265532cd8b3
aeyongdodam <kikititi0510@gmail.com>
2023-11-09 13:14:09 +0900

    #9 feat: email 관련 에러 처리

--- src/main/java/com/example/sharemind/email/exception/InvalidEmailException.java
+++ src/main/java/com/example/sharemind/email/exception/InvalidEmailException.java
@@ -2,6 +2,6 @@ package com.example.sharemind.email.exception;
 
 public class InvalidEmailException extends RuntimeException{
     public InvalidEmailException(String email) {
-        super("유효하지 않은 이메일 형식입니다." + email);
+        super("유효하지 않은 이메일 형식입니다. : " + email);
     }
 }

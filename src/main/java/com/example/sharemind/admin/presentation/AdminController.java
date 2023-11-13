4195204f4152a339fd6cb9c59dfef3213f83859d
letskuku <yujung9288@naver.com>
2023-11-14 03:07:39 +0900

    #10 feat: 관리자 페이지 컨트롤러 구현

---
+++ src/main/java/com/example/sharemind/admin/presentation/AdminController.java
@@ -0,0 +1,30 @@
+package com.example.sharemind.admin.presentation;
+
+import com.example.sharemind.admin.application.AdminService;
+import com.example.sharemind.admin.dto.response.AdminResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v0/admins")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping
+    public ResponseEntity<List<AdminResponse>> getConsultsByIsPayFalse(@RequestParam String password) {
+        return ResponseEntity.ok(adminService.getConsultsByIsPayFalse(password));
+    }
+
+    @PatchMapping("/{consultId}")
+    public ResponseEntity<Void> updateIsPayTrue(@PathVariable Long consultId) {
+
+        adminService.updateIsPayTrue(consultId);
+
+        return ResponseEntity.ok().build();
+    }
+}

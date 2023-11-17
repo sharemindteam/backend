98978427ec8f3e6e368e8ca863775cbea0be4a32
aeyongdodam <kikititi0510@gmail.com>
2023-11-17 14:06:24 +0900

    #14 fix: http method 변경

--- src/main/java/com/example/sharemind/review/presentation/ReviewController.java
+++ src/main/java/com/example/sharemind/review/presentation/ReviewController.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @PostMapping("/{reviewUuid}")
+    @PatchMapping("/{reviewUuid}")
     public ResponseEntity<Void> updateReview(@PathVariable UUID reviewUuid,
                                              @RequestBody UpdateReviewRequest updateReviewRequest) {
         reviewService.updateReview(reviewUuid, updateReviewRequest);
